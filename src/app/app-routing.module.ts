58ff59aad39e81adfa9a1b8c5e74594786ac3594
ianjoseph2204 <44733223+ianjoseph2204@users.noreply.github.com>
2020-08-31 17:32:47 +0700

    Fix Write-Record page conflict

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
 import {LoginComponent} from './pages/login/login.component';
 import {RecordListComponent} from './pages/record-list/record-list.component';
+import {WriteRecordComponent} from './pages/write-record/write-record.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'record-list', component: RecordListComponent}
+  {path: 'record-list', component: RecordListComponent},
+  {path: 'write-record', component: WriteRecordComponent},
 ];
 
 @NgModule({
