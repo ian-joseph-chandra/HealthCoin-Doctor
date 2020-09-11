24268516c53c2418fc18c1cf82568f79bcd4fa0a
Evelyn247 <evelyn82@ui.ac.id>
2020-09-11 14:19:51 +0700

    Revert "Update to ts sign-up page"
    This reverts commit a7fc1403a04040df54f0d04098d8d6adfda4bc93.

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {ApiService} from '../../services/api/api.service';
-import {RouterService} from '../../services/router/router.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -9,74 +7,9 @@ import {RouterService} from '../../services/router/router.service';
 })
 export class SignUpComponent implements OnInit {
 
-  inputFirstName = 'Ian';
-  inputLastName = 'Joseph';
-  inputBirthDate = '1998-04-22';
-  inputEmail = 'ianjoseph.project@gmail.com';
-  inputPhoneNumber = '81513127900';
-  inputPass = 'ian2204';
-  inputPassConfirm = 'ian2204';
-  inputNationalId = '12345';
-
-  /*inputFirstName = '';
-  inputLastName = '';
-  inputBirthDate = '';
-  inputEmail = '';
-  inputPhoneNumber = '';
-  inputPass = '';
-  inputPassConfirm = '';
-  inputNationalId = '';*/
-
-  requestSent = false;
-  error = false;
-  message: string;
-
-  constructor(public router: RouterService, private api: ApiService) {
-  }
+  constructor() { }
 
   ngOnInit(): void {
   }
 
-
-  /**
-   * Check the password confirmation is correct or not. If it's correct, then process the register data.
-   */
-  async signUp() {
-    if (this.inputPass !== this.inputPassConfirm) {
-      this.error = true;
-      this.message = 'Konfirmasi password tidak sesuai!';
-    } else {
-      await this.sendRegisterData();
-
-      // If the request is sent and there isn't any error, then go back to Login page.
-      if (this.requestSent && !this.error) {
-        await this.router.goToLoginPage();
-      }
-    }
-  }
-
-  /**
-   * Send the register data to API server and get the response. Set the error status and message according to the response.
-   */
-  async sendRegisterData() {
-    // Set status for requestSent.
-    this.requestSent = true;
-
-    // Prepare the JSON data to be parsed to API Server
-    const registerJSON = {
-      first_name: this.inputFirstName,
-      last_name: this.inputLastName,
-      birth_date: this.inputBirthDate,
-      email: this.inputEmail,
-      phone_number: this.inputPhoneNumber,
-      pass: this.inputPass,
-      national_id: this.inputNationalId
-    };
-
-    // Send the data to the API server & store the response.
-    const registerResponse = await this.api.sendPostRequest('user-register', registerJSON);
-
-    this.error = registerResponse.error;
-    this.message = registerResponse.message;
-  }
 }
